51a76d988da972def080a378c05c98522d4c414e
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-16 17:21:32 -0400

    Finished Ice Cream 2

--- sketch1/sketch1.js
+++ sketch1/sketch1.js
@@ -28,7 +28,70 @@ function draw(){
     background(255, 237, 244)
     //Using simple shapes to draw unique shapes
     
-    //Ice Cream 1
+    // //Ice Cream 1
+    // noStroke();
+    
+    // //IceCream
+    // fill(252, 86, 141);
+    // ellipse(iceCream.x, iceCream.y, iceCream.r);
+
+    // //Ice Cream cone
+    // fill(145, 82, 0)
+    // triangle(iceCreamCone.x1, iceCreamCone.y1, iceCreamCone.x2, iceCreamCone.y2, iceCreamCone.x3, iceCreamCone.y3);
+
+    // //Ice Cream cone details
+    // stroke(168, 105, 0);
+    // strokeWeight(2);
+    // line(360, 270, 360, 368);
+    // line(340, 270, 340, 319);
+    // line(380, 270, 380, 319);
+    // line(334, 300, 386, 300);
+    // line(346, 335, 374, 335);
+
+    // //IceCream Details
+    // noStroke();
+    // fill(252, 86, 141);
+    // ellipse(325, 270, 25);
+    // ellipse(340, 270, 25);
+    // ellipse(355, 270, 25);
+    // ellipse(370, 270, 25);
+    // ellipse(385, 270, 25);
+    // ellipse(397, 270, 25);
+
+    // //Ice Cream 2
+    // fill(70, 0, 0);
+    // ellipse(iceCream.x, iceCream.y, iceCream.r);
+
+    // //Ice Cream cone
+    // fill(145, 82, 0)
+    // triangle(iceCreamCone.x1, iceCreamCone.y1, iceCreamCone.x2, iceCreamCone.y2, iceCreamCone.x3, iceCreamCone.y3);
+
+    // //Ice Cream cone details
+    // stroke(168, 105, 0);
+    // strokeWeight(2);
+    // line(360, 270, 360, 368);
+    // line(340, 270, 340, 319);
+    // line(380, 270, 380, 319);
+    // line(334, 300, 386, 300);
+    // line(346, 335, 374, 335);
+
+    // //IceCream Details
+    // stroke(255, 0, 0);
+    // strokeWeight(2);
+    // line(340,240,330,230);
+    // line(352,210,362,200);
+    // line(388,230,378,220);
+    // noStroke();
+    // fill(70, 0, 0);
+    // ellipse(325, 270, 25);
+    // ellipse(340, 270, 25);
+    // ellipse(355, 270, 25);
+    // ellipse(370, 270, 25);
+    // ellipse(385, 270, 25);
+    // ellipse(397, 270, 25);
+
+    
+    //Ice Cream 3
     noStroke();
     
     //IceCream
@@ -57,10 +120,4 @@ function draw(){
     ellipse(370, 270, 25);
     ellipse(385, 270, 25);
     ellipse(397, 270, 25);
-
-    //fill(252, 86, 141);
-   // ellipse(360, 240, 100, 100);
-    //fill(70, 0, 0);
-   // ellipse(300, 240, 100, 100);
- 
 }
