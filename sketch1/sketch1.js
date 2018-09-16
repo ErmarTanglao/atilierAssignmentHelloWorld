fadb718494f0ef47e22c84b8fc3581645156794e
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-16 16:47:38 -0400

    Created an ice cream detail variable but failed to work
    manually plugged in the numbers for ice cream details

--- sketch1/sketch1.js
+++ sketch1/sketch1.js
@@ -1,5 +1,6 @@
 var iceCream;
-var cone;
+var iceCreamCone;
+var iceCreamDetails;
 
 function setup(){
     createCanvas(720,480)
@@ -8,6 +9,19 @@ function setup(){
         y: height/2,
         r: 100
     }
+    iceCreamCone = {
+        x1: 320,
+        y1: 270,
+        x2: 400,
+        y2: 270,
+        x3: 360,
+        y3: 370
+    }
+    // iceCreamDetails = {
+    //     x: width/2 - 15,
+    //     y: height/2,
+    //     r: 25
+    // }
 }
 
 function draw(){
@@ -23,22 +37,24 @@ function draw(){
 
     //Ice Cream cone
     fill(145, 82, 0)
-    triangle(320, 270, 400, 270, 360, 370);
+    triangle(iceCreamCone.x1, iceCreamCone.y1, iceCreamCone.x2, iceCreamCone.y2, iceCreamCone.x3, iceCreamCone.y3);
 
     //Ice Cream cone details
     stroke(168, 105, 0);
     strokeWeight(2);
-    line(320, 270, 389, 295);
+    line(360, 270, 360, 368);
+    
+
     
     //IceCream Details
     noStroke();
     fill(252, 86, 141);
-    ellipse(325, 270, 25, 25);
-    ellipse(340, 270, 25, 25);
-    ellipse(355, 270, 25, 25);
-    ellipse(370, 270, 25, 25);
-    ellipse(385, 270, 25, 25);
-    ellipse(397, 270, 25, 25);
+    ellipse(325, 270, 25);
+    ellipse(340, 270, 25);
+    ellipse(355, 270, 25);
+    ellipse(370, 270, 25);
+    ellipse(385, 270, 25);
+    ellipse(397, 270, 25);
 
     //fill(252, 86, 141);
    // ellipse(360, 240, 100, 100);
