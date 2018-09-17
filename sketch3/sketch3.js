d386bd9bcd1f5160e38b11ea370fbc6f072e8143
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-16 21:19:08 -0400

    Added bouncing

--- sketch3/sketch3.js
+++ sketch3/sketch3.js
@@ -1,7 +1,32 @@
-function setup(){
+var ball;
+var w = 720;
+var h = 480;
 
+function setup(){
+    createCanvas(w,h);
+
+    ball = {
+        x: width/2,
+        y: height/2,
+        r: 100,
+        xspeed: 2,
+        yspeed: 2
+    }
 }
 
 function draw(){
-    
+    clear();
+    ellipse(ball.x, ball.y, ball.r);
+    fill(0);
+    ball.x = ball.x + ball.xspeed;
+    ball.y = ball.y + ball.yspeed;
+
+    //Bouncing
+    if (ball.x + ball.r > w || ball.x - ball.r < 0){
+        ball.xspeed = ball.xspeed * (-1)
+    }
+
+    if (ball.y + ball.r > h || ball.y - ball.r < 0){
+        ball.yspeed = ball.yspeed * (-1)
+    }
 }
