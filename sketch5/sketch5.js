23b8fe7e1e49f20684bc275d2269f593ec249ed5
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-17 11:22:57 -0400

    Added a boundary system

--- sketch5/sketch5.js
+++ sketch5/sketch5.js
@@ -1,28 +1,43 @@
-var x = 100;
-var y = 100;
+var ball;
+var w = 720;
+var h = 480;
 
 function setup() {
-  createCanvas(512, 512);
+  createCanvas(w, h);
+  ball = {
+        x: width/2,
+        y: height/2,
+        r: 50
+    }
 }
 
 function draw() {
-  if (keyIsDown(LEFT_ARROW)) {
-    x -= 5;
+    if (keyIsDown(LEFT_ARROW)) {
+        ball.x -= 5;
   }
 
-  if (keyIsDown(RIGHT_ARROW)) {
-    x += 5;
+    if (keyIsDown(RIGHT_ARROW)) {
+        ball.x += 5;
   }
 
-  if (keyIsDown(UP_ARROW)) {
-    y -= 5;
+    if (keyIsDown(UP_ARROW)) {
+        ball.y -= 5;
   }
 
-  if (keyIsDown(DOWN_ARROW)) {
-    y += 5;
+    if (keyIsDown(DOWN_ARROW)) {
+        ball.y += 5;
   }
 
   clear();
+  background(0);
   fill(255, 0, 0);
-  ellipse(x, y, 50, 50);
+  ellipse(ball.x, ball.y, ball.r);
+
+    if (ball.x + ball.r > w || ball.x - ball.r < 0){
+        ball.x = w/2;
+    }
+
+    if (ball.y + ball.r > h || ball.y - ball.r < 0){
+        ball.y = h/2;
+    }   
 }
