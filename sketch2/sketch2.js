5f78d72d4fd7d4caf13408d92eab30752ba8cb6f
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-16 20:00:50 -0400

    Attempt on making the circle a button

--- sketch2/sketch2.js
+++ sketch2/sketch2.js
@@ -1,7 +1,38 @@
+var ball;
+
 function setup(){
     createCanvas(720,480);
+    ball = {
+        x: width/2,
+        y: height/2,
+        r: 100,
+        xspeed: random(-2,2),
+        yspeed: random(-2,2),
+        color: color(random(255), random(255), random(255))
+    }
 }
 
 function draw(){
-    
+    ellipse(ball.x, ball.y, ball.r)
+    // for (let i = 0; i < 10; i++) {
+    //     fill(ball.color);
+    //     ellipse(ball.x, ball.y, ball.r/2);
+    //     ball.x = ball.x + ball.xspeed;
+    //     ball.y = ball.y + ball.yspeed;    
+    // }
+}
+
+function mouseClicked() {
+	if(mouseX < width/2 + 50 && mouseX > width/2 - 50){
+		if(mouseY < height/2 + 50 && mouseY > height/2 - 50){
+            push();
+            for (let i = 0; i < 1; i++) {
+                fill(ball.color);
+                ellipse(random(ball.x), random(ball.y), ball.r/2);
+                ball.x = ball.x + ball.xspeed;
+                ball.y = ball.y + ball.yspeed;    
+            }
+            console.log("Hi");
+        }
+    }
 }
