b734585af2b9de54b148b4cd929dee5482eed5e4
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-17 00:33:21 -0400

    Added rotating circle

--- sketch4/sketch4.js
+++ sketch4/sketch4.js
@@ -1,10 +1,19 @@
-var w = 720;
-var h = 480;
+var radius = 150;
+var angle = 0;
+var speed = 0.05;
+var centerX = 300;
+var centerY = 300;
 
-function setup(){
-    createCavas(w,h);
+function setup() { 
+  createCanvas(600, 600);
 }
 
-function draw(){
+function draw() { 
+    background(220);
+  
+    var x = centerX + radius * cos(angle);
+    var y = centerY + radius * sin(angle);
+    ellipse(x, y, 50, 50);
 
+    angle = angle + speed;
 }
