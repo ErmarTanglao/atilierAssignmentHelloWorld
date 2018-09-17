86bcba37edc5f40b6a3faef1404bffc9e289df89
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-16 22:14:14 -0400

    Redid whole code and added onto the sketch 3 code

--- sketch3/sketch3.js
+++ sketch3/sketch3.js
@@ -1,51 +1,42 @@
-var ball;
-var w = 720;
-var h = 480;
 var mouseclicks = [];
+var w = 720;
+var h = 480
+
 
 function setup(){
-    createCanvas(w,h);
-
-    //Taken from sketch 3
-    var button = createButton("add ball");
-    button.mousePressed(addball);
-    
-    ball = {
-        x: random(width/2),
-        y: random(height/2),
-        r: 100,
-        xspeed: random(-2,2),
-        yspeed: random(-2,2),
-        color: color(random(255), random(255), random(255))
-    }
+	createCanvas(w,h);
+	var button = createButton("Create Ball");
+	button.mousePressed(addball);
+	noStroke();
 }
 
 function draw(){
-    clear();
-    ellipse(ball.x, ball.y, ball.r);
-    fill(ball.color);
-    ball.x = ball.x + ball.xspeed;
-    ball.y = ball.y + ball.yspeed;
-
-    //Bouncing
-    if (ball.x + ball.r > w || ball.x - ball.r < 0){
-        ball.xspeed = ball.xspeed * (-1);
-    }
-
-    if (ball.y + ball.r > h || ball.y - ball.r < 0){
-        ball.yspeed = ball.yspeed * (-1);
+	//translate(width/2,height,2);
+	background(255);
+	for(var i=0; i<mouseclicks.length; i++){
+		fill(mouseclicks[i].color);
+		ellipse(mouseclicks[i].x,mouseclicks[i].y,mouseclicks[i].r);
+		mouseclicks[i].x = mouseclicks[i].x + mouseclicks[i].xspeed;
+        mouseclicks[i].y = mouseclicks[i].y + mouseclicks[i].yspeed;
+        
+        if (mouseclicks[i].x + mouseclicks[i].r > w || mouseclicks[i].x - mouseclicks[i].r < 0){
+            mouseclicks[i].xspeed = mouseclicks[i].xspeed * (-1);
+        }
+    
+        if (mouseclicks[i].y + mouseclicks[i].r > h || mouseclicks[i].y - mouseclicks[i].r < 0){
+            mouseclicks[i].yspeed = mouseclicks[i].yspeed * (-1);
+        }
     }
 }
 
 function addball(){
-    // console.log("Hi")
-	var clickposition = {
-		x: width/2,
-        y: height/2,
-        r: 100,
-        xspeed: random(-2,2),
-        yspeed: random(-2,2),
-        color: color(random(255), random(255), random(255))
+	var ball = {
+		x: random(width),
+        y: random(height),
+        r: 30,
+		xspeed: random(-2,2),
+		yspeed: random(-2,2),
+		color: color(random(255), random(255), random(255))
 	}
-	push(clickposition);
+	mouseclicks.push(ball);
 }
