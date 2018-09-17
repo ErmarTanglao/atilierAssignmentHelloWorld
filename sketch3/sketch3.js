f434ed26ca97f002a38446b38afee6abb2101e7b
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-16 21:42:34 -0400

    .

--- sketch3/sketch3.js
+++ sketch3/sketch3.js
@@ -1,32 +1,51 @@
 var ball;
 var w = 720;
 var h = 480;
+var mouseclicks = [];
 
 function setup(){
     createCanvas(w,h);
 
+    //Taken from sketch 3
+    var button = createButton("add ball");
+    button.mousePressed(addball);
+    
     ball = {
-        x: width/2,
-        y: height/2,
+        x: random(width/2),
+        y: random(height/2),
         r: 100,
-        xspeed: 2,
-        yspeed: 2
+        xspeed: random(-2,2),
+        yspeed: random(-2,2),
+        color: color(random(255), random(255), random(255))
     }
 }
 
 function draw(){
     clear();
     ellipse(ball.x, ball.y, ball.r);
-    fill(0);
+    fill(ball.color);
     ball.x = ball.x + ball.xspeed;
     ball.y = ball.y + ball.yspeed;
 
     //Bouncing
     if (ball.x + ball.r > w || ball.x - ball.r < 0){
-        ball.xspeed = ball.xspeed * (-1)
+        ball.xspeed = ball.xspeed * (-1);
     }
 
     if (ball.y + ball.r > h || ball.y - ball.r < 0){
-        ball.yspeed = ball.yspeed * (-1)
+        ball.yspeed = ball.yspeed * (-1);
     }
+}
+
+function addball(){
+    // console.log("Hi")
+	var clickposition = {
+		x: width/2,
+        y: height/2,
+        r: 100,
+        xspeed: random(-2,2),
+        yspeed: random(-2,2),
+        color: color(random(255), random(255), random(255))
+	}
+	push(clickposition);
 }
